05ecb430cfdb2a183475594652ba164f48c53b3c
Yuwen Li <muzlee1113@gmail.com>
2019-03-06 12:30:04 -0600

    font

--- config/connection.js
+++ config/connection.js
@@ -10,6 +10,7 @@ if (process.env.JAWSDB_URL) {
 } else {
     connection = mysql.createConnection({
         host: "localhost",
+        port: 8889,
         user: "root",
         password: "root",
         database: "burgers_db"
